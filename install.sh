4b1b58e13af8485ed3ae58276d5706c1d739b9bf
Will Tatam <will@netmindz.net>
2023-05-30 12:54:57 +0100

    Update install for linux as well as macos

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
 #cp -v fixtures/* "/Users/lighting/Library/Application Support/QLC+/Fixtures/"
-rm -rf  "/Users/lighting/Library/Application Support/QLC+/Fixtures/"
-ln -s $PWD/fixtures "/Users/lighting/Library/Application Support/QLC+/Fixtures"
+if [ -d /Users ]; then
+	rm -rf  "/Users/lighting/Library/Application Support/QLC+/Fixtures/"
+	ln -s $PWD/fixtures "/Users/lighting/Library/Application Support/QLC+/Fixtures"
+else 
+	rm -rf $HOME/.qlcplus/fixtures
+	ln -s $PWD/fixtures $HOME/.qlcplus/fixtures
+fi
