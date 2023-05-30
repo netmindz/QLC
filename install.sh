4524120928e52d582003aa78dc426e6c4e62f9e5
Will Tatam <will@netmindz.net>
2023-05-30 04:22:50 -0700

    Instal fixtures

---
+++ install.sh
@@ -0,0 +1,3 @@
+#cp -v fixtures/* "/Users/lighting/Library/Application Support/QLC+/Fixtures/"
+rm -rf  "/Users/lighting/Library/Application Support/QLC+/Fixtures/"
+ln -s $PWD/fixtures "/Users/lighting/Library/Application Support/QLC+/Fixtures"
